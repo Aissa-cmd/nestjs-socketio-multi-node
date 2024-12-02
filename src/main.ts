82764d355c37ddf391a3456f547eab87a6a3fc6a
Aissa Ouboukioud <aissa.1bacsm1@gmail.com>
2024-12-02 22:07:26 +0100

    add redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './chat/redis-adapter';
 
 async function bootstrap() {
+  const logger = new Logger('main.ts');
   const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  logger.log('Connected to redis');
+  app.useWebSocketAdapter(redisIoAdapter);
+
   await app.listen(3000);
 }
 bootstrap();
